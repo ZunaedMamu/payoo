eaec8f6648bb1aaaa96c9df7e2ece7669f8a5dab
ZunaedMamu <zunaied19988@gmail.com>
2024-09-25 02:30:03 +0600

    2nd commit

---
+++ js/login.js
@@ -0,0 +1,20 @@
+console.log("button clicking file added");
+document.getElementById("btn").addEventListener("click", function(event){
+    event.preventDefault();
+    // console.log ("login button clicked");
+
+    const phone = document.getElementById("phone-number").value;
+    console.log (phone);
+
+    const pin = document.getElementById("pin").value;
+    console.log (pin);
+
+    if(phone === "01845682811" && pin === "1234"){
+        alert("Login sucessfull");
+        window.location.href = "../home.html";
+    }
+    else{
+        // Alert 
+        alert("Wrong Input");
+    }
+})
